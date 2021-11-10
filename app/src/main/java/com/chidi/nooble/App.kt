5eff5cc71d9af05dc3a385c8a779f2448bdb3e3d
Ngwuta Desmond <desmondmuqsit@yahoo.com>
2021-11-10 21:01:21 +0100

    (update|improvement)
    added ExoPlayer to App Module: this will be used for playing or streaming the audio in each short
    
    also created an App class which is the base application class

---
+++ app/src/main/java/com/chidi/nooble/App.kt
@@ -0,0 +1,27 @@
+package com.chidi.nooble
+
+import android.app.Application
+import com.google.android.exoplayer2.database.DatabaseProvider
+import com.google.android.exoplayer2.database.StandaloneDatabaseProvider
+import com.google.android.exoplayer2.upstream.cache.LeastRecentlyUsedCacheEvictor
+import com.google.android.exoplayer2.upstream.cache.SimpleCache
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application() {
+    companion object {
+        var simpleCache: SimpleCache? = null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val leastRecentlyUsedCacheEvictor = LeastRecentlyUsedCacheEvictor(90 * 1024 * 1024)
+        val databaseProvider: DatabaseProvider = StandaloneDatabaseProvider(this)
+
+        if (simpleCache == null) {
+            simpleCache = SimpleCache(cacheDir, leastRecentlyUsedCacheEvictor, databaseProvider)
+        }
+    }
+
+}
